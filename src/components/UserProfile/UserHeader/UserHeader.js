022a40e248fa9502e57712b48d8f8985c68c65bd
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-06-10 00:41:45 +0300

    edited UserHeader button style

--- src/components/UserProfile/UserHeader/UserHeader.js
+++ src/components/UserProfile/UserHeader/UserHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './UserHeader.css';
+// import userImg from '../../../pictures/square-image.png'
 
 const UserHeader = (props) => {
 
@@ -9,9 +10,9 @@ const UserHeader = (props) => {
             <div className="header">
                 <h1 className="user-name">{props.userName}</h1>
                 <div className="user-image">
-                    {props.myProfile ? <button className="user-pic-icon"><i className="fas fa-camera-retro"></i></button> : null}
+                    {props.myProfile ? <button className="circular ui icon button"><i className="fas fa-camera-retro"></i></button> : null}
                 </div>
-                {props.myProfile ? <button className="user-header-pic-icon"><i className="fas fa-camera-retro"></i>Change header picture</button> : null}
+                {props.myProfile ? <button className="ui button icon userHeaderBtn"><i className="fas fa-camera-retro"></i>Change header picture</button> : null}
             </div>
         </div>
     )
