7b43387f0e8272d196221380f0fdfec943865f09
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-06-15 17:50:09 +0300

    started to implement UserFollowingSection

--- src/components/UserProfile/UserFollowingSection/UserFollowingSection.js
+++ src/components/UserProfile/UserFollowingSection/UserFollowingSection.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import './UserFollowingSection.css';
+import facebookApi from '../../../apis/facebook-api';
+import Cookies from 'universal-cookie';
 
 class UserFollowingSection extends React.Component {
+
+    state = { followinglist: [] }
+
+    async componentDidMount() {
+        const cookies = new Cookies();
+        const userToken = cookies.get('userToken');
+
+        const res = await facebookApi.get('/users/me', {
+            headers: { Authorization: "Bearer " + userToken }
+        });
+        const data = [...res.data.usersIFollow];
+        const followinglist = [];
+
+        data.forEach(async(following)=> {
+            try {
+                const res = await facebookApi.get(`/users/${following.userId}`)
+                followinglist.push(res.data[0]);
+            } catch(e) {
+                console.log(e);
+            }
+        })
+        this.setState({followinglist: [...followinglist]})
+    }
+
+    renderFollowingList = () => {
+        return (
+            this.state.followinglist.map((following)=> {
+                return (
+                    <div className="follow">
+                        <p>{`${following.firstName} ${following.lastName}`}</p>
+                    </div>
+                )
+            })
+        )
+    }
 
     render() {
         return (
             <div>
                 <h3 className="followingh2">Users I follow</h3>
                 <div className="followingContainer">
-                    <div className="follow"></div>
-                    <div className="follow"></div>
-                    <div className="follow"></div>
-                    <div className="follow"></div>
-                    <div className="follow"></div>
-                    <div className="follow"></div>
-                    <div className="follow"></div>
-                    <div className="follow"></div>
-                    <div className="follow"></div>
-                    <div className="follow"></div>
-                    <div className="follow"></div>
+                    {this.state.followinglist.length !== 0 ? this.renderFollowingList() : null}
                 </div>
             </div>
         )
