96ad4931e60e1af169df401467559f0aa3816fec
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-07-03 15:56:38 +0300

    rendered feed posts on UserFeed

--- src/components/UserFeed/UserFeed.js
+++ src/components/UserFeed/UserFeed.js
@@ -1,8 +1,84 @@
 import React from 'react';
 import './UserFeed.css';
 import UserNavBar from '../UserProfile/UserNavBar/UserNavBar';
+import facebookApi from '../../apis/facebook-api';
+import Cookies from 'universal-cookie';
 
 class UserFeed extends React.Component {
+
+    state = { myFollowing: [], posts: [] }
+
+    async componentDidMount() {
+        const cookies = new Cookies();
+        const userToken = cookies.get('userToken');
+        try {
+            const res = await facebookApi.get('/users/me', {
+                headers: { Authorization: "Bearer " + userToken }
+            });
+            console.log(res.data.usersIFollow)
+            this.setState({ myFollowing: [...res.data.usersIFollow] })
+            this.fetchPostsFromMyFollowing();
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    fetchPostsFromMyFollowing = async () => {
+        console.log("innn")
+        const myFollowing = this.state.myFollowing;
+        const posts = [];
+        for (let i = 0; i < myFollowing.length; i++) {
+            const currentFollowingPosts = await this.fetchUsers(myFollowing, i);
+            posts.push(...currentFollowingPosts);
+        }
+
+        this.setState({posts:[...posts]})
+    }
+
+    fetchUsers = async (data, i) => {
+        try {
+            const res = await facebookApi.get(`/posts/user/${data[i].userId}`);
+            const res2 = await facebookApi.get(`/users/id/${data[i].userId}`);
+            const posts = res.data.map((post)=> {
+                return (
+                    {postBody: post.postBody, postHeader:post.postHeader, owner: `${res2.data.firstName} ${res2.data.lastName}`, path: res2.data.path}
+                )
+            })
+            return posts;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    renderPostsToFeed = () => {
+        return (
+            this.state.posts.map((post) => {
+                return (
+                    <div className="ui card" key={post._id}>
+                        <div className="content scroll">
+                            <h4><a href={`/users/${post.path}`} className="userLink"><i className="user icon"></i>{post.owner}</a></h4>
+                            {post.postHeader ? <h3 className="postHeader">{post.postHeader}</h3> : null}
+                            <div className="postBody">
+                                <p className="postBody">{post.postBody}</p>
+                            </div>
+                        </div>
+                        
+                        <div className="extra content">
+                            <span className="left floated like">
+                                <i className="like icon"></i>
+                                Like</span>
+                            <span >
+                                <i className="comment icon"></i>
+                                Comments</span>
+                            <span className="right floated star">
+                                <i className="star icon"></i>
+                                Favorite</span>
+                        </div>
+                    </div>
+                )
+            })
+        )
+    }
 
     render() {
         return (
@@ -10,8 +86,9 @@ class UserFeed extends React.Component {
                 <UserNavBar />
                 <div className="feedContainer">
                     <div>
-                        friends posts
-                </div>
+                        <h2 className="feedHeader">My Feed</h2>
+                        {this.state.posts.length !== 0 ? this.renderPostsToFeed():null}
+                    </div>
                 </div>
 
             </div>
